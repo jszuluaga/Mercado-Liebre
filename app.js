1d2a08942af8f38fe09a98358b182e9440ef7280
jszuluaga <johanas_zuluaga@hotmail.com>
2022-06-20 19:36:08 -0500

    modificacion js

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const path = require('path');
 // app.use('/static', express.static(__dirname + '/public'));
 app.use(express.static('public'));
 
-app.listen(3001, () => {
+app.set('puerto',process.env.PORT || 3000)
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
+
+
+/*app.listen(3001, () => {
   console.log("Servidor Corriendo en puerto 3001")
-})
+})*/
 
 app.get('/',(req,res) => { 
   res.sendFile(path.join(__dirname,"/views/home.html"))
